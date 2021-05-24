d4f642f4a53c39d3e28f58d8658605ccf5ba443c
Alejandro <alex@gomez.com>
2021-05-24 09:54:26 +0200

    javadoc

--- ControlVersiones/src/controlVersiones/Empleado.java
+++ ControlVersiones/src/controlVersiones/Empleado.java
@@ -1,11 +1,26 @@
 package controlVersiones;
 
+/**
+ * @author Alejandro
+ *@version 1, 2021
+ *@since 24/05/2021
+ */
 public class Empleado {
+	/**
+	 * Nombre del empleado
+	 * Apellidos del empleado
+	 * Salario del empleado
+	 */
 	private String nombre;
 	private String apellidos;
 	private double salario; 
 
 
+/**
+ * @param nombre
+ * @param apellido
+ * @param salario
+ */
 public Empleado(String nombre, String apellido, double salario) {
 	this.nombre = nombre;
 	this.apellidos = apellido;
@@ -33,6 +48,9 @@ public void setSalario(double salario) {
 	this.salario = salario;
 }
 
+/**
+ * Aumenta el salario del empleado
+ */
 public void subirsalario (double subida) {
 	salario=salario+subida;
 }
